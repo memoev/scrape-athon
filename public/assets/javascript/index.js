2872a2da9a2cf7e6a37bbd2c6834f7a45c378651
Guillermo Villalta <mexcelus@gmail.com>
2019-11-12 03:49:39 -0700

    patch bug fix

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
         articleToSave.saved= true;
 
         $.ajax({
-            method: PATCH,
+            method: "PATCH",
             url: "/api/headlines",
             data: articleToSave
         })
