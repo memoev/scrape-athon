693c6fe1e1f4b73b87878b64dd1a686219779027
Guillermo Villalta <mexcelus@gmail.com>
2019-11-11 22:42:15 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+// Exoress - Router
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app =  express();
+
+var router = express.Router();
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({defaultLayout: "main"}))
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(router);
+
+app.listen(PORT, function() {
+    console.log("Listening on port" + PORT);
+});
